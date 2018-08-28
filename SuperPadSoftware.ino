8fdda78b3bffa797fbfa695554879f87eb550697
BambooEngineer <37255295+BambooEngineer@users.noreply.github.com>
2018-08-28 14:08:20 -0400

    Add files via upload

---
+++ SuperPadSoftware.ino
@@ -0,0 +1,69 @@
+int LeftD = 6; // Pulled HIGH Digital Input Pins
+int RightD = 7; 
+int Left = 4;  
+int Right = 5; 
+                                                                
+
+
+int up = A3; // Potentiometer Middle Pin  + DIODES ARE PLACED ON THIS PIN SO THE ARDUINO IS NOT DAMAGED //
+int Vv = A5; // Potentiometer Voltage Pin //
+int right = 10; // Potentiometer Middle Pin + DIODES ARE PLACED ON THIS PIN SO THE ARDUINO IS NOT DAMAGED //
+int Hv = 12; // Output Voltage Pin // 
+
+int L = 0;
+int R = 0;
+int l = 0;
+int r = 0;
+
+void setup() {
+  
+  pinMode(LeftD, INPUT_PULLUP);
+  pinMode(RightD, INPUT_PULLUP);
+  pinMode(Left, INPUT_PULLUP); 
+  pinMode(Right, INPUT_PULLUP);
+  pinMode(up, OUTPUT);
+  pinMode(right, OUTPUT);
+  pinMode(Vv, OUTPUT);
+  pinMode(Hv, OUTPUT);
+  
+}
+
+void loop() {
+
+ L = digitalRead(LeftD); 
+ R = digitalRead(RightD);
+ l = digitalRead(Left); 
+ r = digitalRead(Right); 
+
+
+ if(R==0){
+    digitalWrite(right,HIGH );
+  }
+ else{
+    digitalWrite(right,LOW);
+  }
+
+
+ if(L==0){
+    digitalWrite(Hv,LOW);
+  }
+ else{
+    digitalWrite(Hv,HIGH);
+ }
+
+ if(r==0){ 
+    analogWrite(up,HIGH );
+  }
+ else{
+    analogWrite(up,LOW);
+  }
+
+
+  if(l==0){ 
+    analogWrite(Vv,LOW);
+  }
+ else{
+    analogWrite(Vv,HIGH);
+ }
+
+}
